84cf0bfe295460ec2226c659184dafcf8210e7bd
Dave Hulbert <dave1010@gmail.com>
2025-11-21 17:08:22 +0000

    Remove inline tests from InlineTest model

--- src/Comparator/EqualityComparator.php
+++ src/Comparator/EqualityComparator.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace IT\Comparator;
 
+use IT\Should;
+
 class EqualityComparator implements ComparatorInterface
 {
+    #[Should(return: true, given: [[1], [1]])]
+    #[Should(return: false, given: [[1], ['1']])]
     public function isEqual(mixed $expected, mixed $actual): bool
     {
         return $expected === $actual;
