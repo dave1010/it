e911e1fe2558893c99919c4fbd040986b5317a7a
Dave Hulbert <dave1010@gmail.com>
2025-12-01 23:47:29 +0000

    Add more inline examples and comparator tests

--- src/Comparator/EqualityComparator.php
+++ src/Comparator/EqualityComparator.php
@@ -10,6 +10,8 @@ class EqualityComparator implements ComparatorInterface
 {
     #[Should(return: true, given: [[1], [1]])]
     #[Should(return: false, given: [[1], ['1']])]
+    #[Should(return: true, given: [[['one' => 1, 'two' => 2]], [['one' => 1, 'two' => 2]]])]
+    #[Should(return: false, given: [[[1, 2, 3]], [[3, 2, 1]]])]
     public function isEqual(mixed $expected, mixed $actual): bool
     {
         return $expected === $actual;
